8e8bb0d7821d57e5896e6cf176f23b5cde99a233
Nedu <nedumrobert@gmail.com>
2021-10-24 11:18:50 -0400

    clean up client

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react'
+// import logo from './logo.svg'
+import './App.css'
+
+const App: FC = () => {
+    return <div className="App">Testing Testing</div>
+}
+
+export default App
